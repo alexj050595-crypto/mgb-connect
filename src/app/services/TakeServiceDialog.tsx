64577f078bab51d7bd1e86f9ecc96375bad9fcbc
alexj050595-crypto <alexj.050595@gmail.com>
2026-09-23 15:10:15 +0200

    Optimize service dialogs for phone screens

--- src/app/services/TakeServiceDialog.tsx
+++ src/app/services/TakeServiceDialog.tsx
@@ -44,7 +44,9 @@ export default function TakeServiceDialog({
         className="
           relative
           z-10
-          w-full
+          w-[calc(100vw-1.5rem)]
+          max-h-[calc(100dvh-1.5rem)]
+          overflow-y-auto
           max-w-lg
           overflow-hidden
           rounded-[28px]
